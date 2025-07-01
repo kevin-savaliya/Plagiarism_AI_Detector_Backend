f0ed8763d7aa5950c5ce341da654480d49fb6493
Kevin Savaliya <kevinsavaliya171@gmail.com>
2025-07-01 10:48:37 +0530

    Fix python version-6

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 apt-get update
-apt-get install -y build-essential
+apt-get install -y libpython3.11-dev build-essential libblas-dev liblapack-dev gfortran
+rm -rf /var/lib/apt/lists/*
