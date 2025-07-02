4e22f71d5e0ca49a40ca3fca65d444488c409319
Kevin Savaliya <kevinsavaliya171@gmail.com>
2025-07-02 10:43:08 +0530

    Add NLTK punkt and wordnet data download to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 apt-get update
-apt-get install -y libpython3.11-dev build-essential libblas-dev liblapack-dev gfortran
+apt-get install -y libpython3.13-dev build-essential libxml2-dev libxslt1-dev libmagic-dev
 rm -rf /var/lib/apt/lists/*
+
+# Install NLTK data
+python -c "import nltk; nltk.download('punkt', download_dir='/opt/render/nltk_data')"
+python -c "import nltk; nltk.download('wordnet', download_dir='/opt/render/nltk_data')"
